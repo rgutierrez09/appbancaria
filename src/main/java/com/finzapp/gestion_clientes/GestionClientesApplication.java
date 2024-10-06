d65e73d9276525bafbc4476215f019efee59ca0e
Ronald Gutierrez Garzon <ronaldgutierrez10@gmail.com>
2024-10-05 21:39:48 -0500

    Estructura del proyecto

---
+++ src/main/java/com/finzapp/gestion_clientes/GestionClientesApplication.java
@@ -0,0 +1,13 @@
+package com.finzapp.gestion_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finzapp/gestion_clientes/GestionClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finzapp.gestion_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
